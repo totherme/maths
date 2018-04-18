22927e48d069e7cd76747258ea778f483a1ee38d
Gareth Smith <gsmith@pivotal.io>
2018-04-18 21:27:50 +0100

    Start to play with foldPairwise

---
+++ src/Lib.hs
@@ -0,0 +1,10 @@
+module Lib where
+
+import Control.Monad.State.Lazy
+
+
+foldPairwise :: (a -> a -> a) -> [a] -> [a]
+foldPairwise f = snd . foldl g (Nothing, []) where
+  -- g :: (Maybe a, [a]) -> a -> (Maybe a, [a]) -- how to bind this 'a' to the a above?
+  g (Nothing, []) x = (Just x, [])
+  g (Just y, xs) x = (Just x, f x y : xs)
