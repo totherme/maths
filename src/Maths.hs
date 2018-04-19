52a9ce9837f1044833cd09cb0eb3db0636000f08
Gareth Smith <gsmith@pivotal.io>
2018-04-19 21:14:18 +0100

    Test relationship between foldPairwise and snd

--- src/Maths.hs
+++ src/Maths.hs
@@ -1,4 +1,4 @@
-module Lib where
+module Maths where
 
 import Control.Monad.State.Lazy
 
@@ -7,4 +7,4 @@ foldPairwise :: (a -> a -> a) -> [a] -> [a]
 foldPairwise f = snd . foldl g (Nothing, []) where
   -- g :: (Maybe a, [a]) -> a -> (Maybe a, [a]) -- how to bind this 'a' to the a above?
   g (Nothing, []) x = (Just x, [])
-  g (Just y, xs) x = (Just x, f x y : xs)
+  g (Just y, xs) x = (Just x, xs ++ [f y x])
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,4 +1,14 @@
-import Data.Text
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import Test.Hspec
+import Maths
+import Test.QuickCheck (property, forAll, listOf1, arbitrary)
 
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "FoldPairwise" $ do
+    it "reduces length by 1" $ forAll (listOf1 arbitrary) 
+      (\(list :: [Int]) -> length (foldPairwise (+) list) `shouldBe` length list - 1)
+    context "when called with 'curry snd'" $ do
+      it "equals tail" $ forAll (listOf1 arbitrary) 
+        (\(list :: [Int]) -> foldPairwise (curry snd) list `shouldBe` tail list)
