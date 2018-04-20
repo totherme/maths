8828f4b33973d2d9e131d2c8bd74153181cb48b1
Gareth Smith <gsmith@pivotal.io>
2018-04-20 19:46:55 +0100

    Add alternative foldPairwise implementations

--- src/Maths.hs
+++ src/Maths.hs
@@ -8,3 +8,14 @@ foldPairwise f = snd . foldl g (Nothing, []) where
   -- g :: (Maybe a, [a]) -> a -> (Maybe a, [a]) -- how to bind this 'a' to the a above?
   g (Nothing, []) x = (Just x, [])
   g (Just y, xs) x = (Just x, xs ++ [f y x])
+
+foldPairwiseRight :: (a -> a -> a) -> [a] -> [a]
+foldPairwiseRight f = snd . foldr g (Nothing, []) where
+  g x (Nothing, []) = (Just x, [])
+  g x (Just y, xs) = (Just x, f x y : xs)
+
+foldPairwiseRec :: (a -> a -> a) -> [a] -> [a]
+foldPairwiseRec f [] = []
+foldPairwiseRec f xs = foldPairwiseRec' f (head xs) (tail xs) where
+  foldPairwiseRec' f _ [] = []
+  foldPairwiseRec' f x (y:ys) = f x y : foldPairwiseRec' f y ys
--- test/Spec.hs
+++ test/Spec.hs
@@ -7,13 +7,19 @@ import Test.QuickCheck (property, forAll, listOf1, arbitrary)
 main :: IO ()
 main = hspec $ do
   describe "FoldPairwise" $ do
-    it "reduces length by 1" $ forAll (listOf1 arbitrary) 
-      (\(list :: [Int]) -> length (foldPairwise (+) list) `shouldBe` length list - 1)
+    it "reduces length by 1" $ forAll (listOf1 arbitrary) $
+      \(list :: [Int]) -> length (foldPairwise (+) list) `shouldBe` length list - 1
     context "when called with 'curry snd'" $ do
-      it "equals tail" $ forAll (listOf1 arbitrary) 
-        (\(list :: [Int]) -> foldPairwise (curry snd) list `shouldBe` tail list)
+      it "equals tail" $ forAll (listOf1 arbitrary) $
+        \(list :: [Int]) -> foldPairwise (curry snd) list `shouldBe` tail list
     context "when called with 'curry fst'" $ do
-      it "equals reverse.tail.reverse" $ forAll (listOf1 arbitrary) 
-        (\(list :: [Int]) -> foldPairwise (curry fst) list 
+      it "equals reverse.tail.reverse" $ forAll (listOf1 arbitrary) $
+        \(list :: [Int]) -> foldPairwise (curry fst) list
           `shouldBe` 
-          (reverse . tail . reverse) list)
+          (reverse . tail . reverse) list
+  describe "foldr variation" $ do
+    it "works like the foldl one" $ property $
+      \(list :: [Int]) -> foldPairwiseRight (+) list `shouldBe` foldPairwise (+) list
+  describe "explicitly recursive variation" $ do
+    it "works like the foldl one" $ property $
+      \(list :: [Int]) -> foldPairwiseRec (+) list `shouldBe` foldPairwise (+) list
