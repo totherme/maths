01b0bc88ee0fcb7a89b61f3da2b80c0dcbf5f277
Gareth Smith <gsmith@pivotal.io>
2018-04-20 21:53:46 +0100

    Start making foldPairwise more generic

--- src/Maths.hs
+++ src/Maths.hs
@@ -2,19 +2,23 @@ module Maths where
 
 import Control.Monad.State.Lazy
 
-
-foldPairwise :: (a -> a -> a) -> [a] -> [a]
+foldPairwise :: (a -> a -> b) -> [a] -> [b]
 foldPairwise f = snd . foldl g (Nothing, []) where
   -- g :: (Maybe a, [a]) -> a -> (Maybe a, [a]) -- how to bind this 'a' to the a above?
   g (Nothing, []) x = (Just x, [])
   g (Just y, xs) x = (Just x, xs ++ [f y x])
 
-foldPairwiseRight :: (a -> a -> a) -> [a] -> [a]
+foldPairwiseRight :: (a -> a -> b) -> [a] -> [b]
 foldPairwiseRight f = snd . foldr g (Nothing, []) where
   g x (Nothing, []) = (Just x, [])
   g x (Just y, xs) = (Just x, f x y : xs)
 
-foldPairwiseRec :: (a -> a -> a) -> [a] -> [a]
+foldPairWiseGen :: Foldable t => (a -> a -> b) -> t a -> [b]
+foldPairWiseGen f = snd . foldr g (Nothing, []) where
+  g x (Nothing, []) = (Just x, [])
+  g x (Just y, xs) = (Just x, f x y : xs)
+
+foldPairwiseRec :: (a -> a -> b) -> [a] -> [b]
 foldPairwiseRec f [] = []
 foldPairwiseRec f xs = foldPairwiseRec' f (head xs) (tail xs) where
   foldPairwiseRec' f _ [] = []
--- test/Spec.hs
+++ test/Spec.hs
@@ -9,10 +9,10 @@ main = hspec $ do
   describe "FoldPairwise" $ do
     it "reduces length by 1" $ forAll (listOf1 arbitrary) $
       \(list :: [Int]) -> length (foldPairwise (+) list) `shouldBe` length list - 1
-    context "when called with 'curry snd'" $ do
+    context "when called with 'curry snd'" $
       it "equals tail" $ forAll (listOf1 arbitrary) $
         \(list :: [Int]) -> foldPairwise (curry snd) list `shouldBe` tail list
-    context "when called with 'curry fst'" $ do
+    context "when called with 'curry fst'" $
       it "equals reverse.tail.reverse" $ forAll (listOf1 arbitrary) $
         \(list :: [Int]) -> foldPairwise (curry fst) list
           `shouldBe` 
@@ -23,3 +23,6 @@ main = hspec $ do
   describe "explicitly recursive variation" $ do
     it "works like the foldl one" $ property $
       \(list :: [Int]) -> foldPairwiseRec (+) list `shouldBe` foldPairwise (+) list
+  describe "generic version" $ do
+    it "works like the foldl one" $ property $
+      \(list :: [Int]) -> foldPairWiseGen (+) list `shouldBe` foldPairwise (+) list
