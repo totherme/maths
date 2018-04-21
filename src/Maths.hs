3f686fc736bbb4a7376741e5b4a2b4505505e205
Gareth Smith <gsmith@pivotal.io>
2018-04-21 19:06:42 +0100

    Use the general version by default
    The less general implementations are still in the git history, but the
    general one seems most useful.

--- src/Maths.hs
+++ src/Maths.hs
@@ -5,21 +5,9 @@ module Maths where
 import Data.Unfoldable
 import Data.Maybe
 
-foldPairwise :: forall a b . (a -> a -> b) -> [a] -> [b]
-foldPairwise f = snd . foldl g (Nothing, []) where
-  g :: (Maybe a, [b]) -> a -> (Maybe a, [b])
-  g (Nothing, []) x = (Just x, [])
-  g (Just y, xs) x = (Just x, xs ++ [f y x])
-
-foldPairwiseRight :: forall a b . (a -> a -> b) -> [a] -> [b]
-foldPairwiseRight f = snd . foldr g (Nothing, []) where
-  g :: a -> (Maybe a, [b]) -> (Maybe a, [b])
-  g x (Nothing, []) = (Just x, [])
-  g x (Just y, xs) = (Just x, f x y : xs)
-
-foldPairWiseGeneral :: forall a b t . (Foldable t , Unfoldable t) => 
-                       (a -> a -> b) -> t a -> t b
-foldPairWiseGeneral f = h . snd . foldr g (Nothing, []) where
+foldPairWise :: forall a b t . (Foldable t , Unfoldable t) =>
+                (a -> a -> b) -> t a -> t b
+foldPairWise f = h . snd . foldr g (Nothing, []) where
   g :: a -> (Maybe a, [b]) -> (Maybe a, [b])
   g x (Nothing, []) = (Just x, [])
   g x (Just y, xs) = (Just x, f x y : xs)
--- test/Spec.hs
+++ test/Spec.hs
@@ -8,29 +8,22 @@ import GHC.Generics
 import Data.Unfoldable
 
 main :: IO ()
-main = hspec $ do
+main = hspec $
   describe "FoldPairwise" $ do
     it "reduces length by 1" $ forAll (listOf1 arbitrary) $
-      \(list :: [Int]) -> length (foldPairwise (+) list) `shouldBe` length list - 1
+      \(list :: [Int]) -> length (foldPairWise (+) list) `shouldBe` length list - 1
     context "when called with 'curry snd'" $
       it "equals tail" $ forAll (listOf1 arbitrary) $
-        \(list :: [Int]) -> foldPairwise (curry snd) list `shouldBe` tail list
+        \(list :: [Int]) -> foldPairWise (curry snd) list `shouldBe` tail list
     context "when called with 'curry fst'" $
       it "equals init" $ forAll (listOf1 arbitrary) $
-        \(list :: [Int]) -> foldPairwise (curry fst) list
+        \(list :: [Int]) -> foldPairWise (curry fst) list
           `shouldBe` 
           init list
-  describe "foldr variation" $
-    it "works like the foldl one" $ property $
-      \(list :: [Int]) -> foldPairwiseRight (+) list `shouldBe` foldPairwise (+) list
-  describe "explicitly recursive variation" $
-    it "works like the foldl one" $ property $
-      \(list :: [Int]) -> foldPairwiseSpec (+) list `shouldBe` foldPairwise (+) list
-  describe "generic version" $ do
-    it "works like the foldl one" $ property $
-      \(list :: [Int]) -> foldPairWiseGeneral (+) list `shouldBe` foldPairwise (+) list
+    it "works like the list-only spec" $ property $
+      \(list :: [Int]) -> foldPairWise (+) list `shouldBe` foldPairwiseSpec (+) list
     it "works on trees as well as lists" $
-      foldPairWiseGeneral (+) (Node (Node (Leaf 1) (Leaf 1)) (Leaf 1))
+      foldPairWise (+) (Node (Node (Leaf 1) (Leaf 1)) (Leaf 1))
       `shouldBe`
       Node (Leaf 2) (Leaf 2)
 
