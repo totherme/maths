0e3fd603c93f067d8c647cd8b9ecc2ef9d9bb28f
Gareth Smith <gsmith@pivotal.io>
2018-04-21 17:50:36 +0100

    Use scoped type variables to type where clauses

--- src/Maths.hs
+++ src/Maths.hs
@@ -1,25 +1,30 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Maths where
 
 import Control.Monad.State.Lazy
 
-foldPairwise :: (a -> a -> b) -> [a] -> [b]
+foldPairwise :: forall a b . (a -> a -> b) -> [a] -> [b]
 foldPairwise f = snd . foldl g (Nothing, []) where
-  -- g :: (Maybe a, [a]) -> a -> (Maybe a, [a]) -- how to bind this 'a' to the a above?
+  g :: (Maybe a, [b]) -> a -> (Maybe a, [b])
   g (Nothing, []) x = (Just x, [])
   g (Just y, xs) x = (Just x, xs ++ [f y x])
 
-foldPairwiseRight :: (a -> a -> b) -> [a] -> [b]
+foldPairwiseRight :: forall a b . (a -> a -> b) -> [a] -> [b]
 foldPairwiseRight f = snd . foldr g (Nothing, []) where
+  g :: a -> (Maybe a, [b]) -> (Maybe a, [b])
   g x (Nothing, []) = (Just x, [])
   g x (Just y, xs) = (Just x, f x y : xs)
 
-foldPairWiseGen :: Foldable t => (a -> a -> b) -> t a -> [b]
+foldPairWiseGen :: forall a b t . Foldable t => (a -> a -> b) -> t a -> [b]
 foldPairWiseGen f = snd . foldr g (Nothing, []) where
+  g :: a -> (Maybe a, [b]) -> (Maybe a, [b])
   g x (Nothing, []) = (Just x, [])
   g x (Just y, xs) = (Just x, f x y : xs)
 
-foldPairwiseRec :: (a -> a -> b) -> [a] -> [b]
+foldPairwiseRec :: forall a b . (a -> a -> b) -> [a] -> [b]
 foldPairwiseRec f [] = []
 foldPairwiseRec f xs = foldPairwiseRec' f (head xs) (tail xs) where
+  foldPairwiseRec' :: (a -> a -> b) -> a -> [a] -> [b]
   foldPairwiseRec' f _ [] = []
   foldPairwiseRec' f x (y:ys) = f x y : foldPairwiseRec' f y ys
