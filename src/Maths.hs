84ffc5bcffd9b07161d5bc728aa0496dee47abe2
Gareth Smith <gsmith@pivotal.io>
2018-04-21 19:28:17 +0100

    Inline a function that turned out to be simple
    Having an explicit type signature for h was kinda nice, but the flow
    of the function composition is clearer without the indirection, I think.

--- src/Maths.hs
+++ src/Maths.hs
@@ -7,9 +7,7 @@ import Data.Maybe
 
 foldPairWise :: forall a b t . (Foldable t , Unfoldable t) =>
                 (a -> a -> b) -> t a -> t b
-foldPairWise f = h . snd . foldr g (Nothing, []) where
+foldPairWise f = fromJust . fromList . snd . foldr g (Nothing, []) where
   g :: a -> (Maybe a, [b]) -> (Maybe a, [b])
   g x (Nothing, []) = (Just x, [])
   g x (Just y, xs) = (Just x, f x y : xs)
-  h :: [b] -> t b
-  h = fromJust . fromList
